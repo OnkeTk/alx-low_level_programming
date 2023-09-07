13f46cecfb4b38ddf73c2724b786a6a35826b54f
OnkeTk <onkemetsetikane@gmail.com>
2023-09-07 15:27:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-#include <stdioh>
-
-
+#include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual"
- * puzzle, followed by a newline."
- * return 0.
+ *  main - this is the main function
+ *  Return: always 0
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
