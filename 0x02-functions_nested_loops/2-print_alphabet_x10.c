a328040a4bc3ae515cd85c8e3c1d0e0a5bd0cb89
OnkeTk <onkemetsetikane@gmail.com>
2023-09-12 07:29:45 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphanet_x10 - Make alphabet x10 time
+ * print_alphanet_x10 - Prints the lowercase English alphabet
+ * from 'a' to 'z' ten times,
+ * with each set of letters separated by a newline character.
  * Return: void
  */
 
@@ -9,17 +11,17 @@ void print_alphabet_x10(void)
 
 {
 
-	char c;
-	int i = 0;
+char c;
+int i = 0;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-			_putchar('\n');
-			i++;
-		}
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
 
 }
