e85927515bfede4a3522a51c443b217396533742
OnkeTk <onkemetsetikane@gmail.com>
2024-03-29 12:41:12 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input,
- * starting with 0
- * @n: The value of the times table to be printed.
- */
+  * print_times_table - Print time tables as specified
+  * @n: The number of times the table would be printed
+  * Returns: 0 (success)
+  */
 
 void print_times_table(int n)
-
 {
 	int num, mult, prod;
 
@@ -16,25 +15,28 @@ void print_times_table(int n)
 		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-			for (mult = 1; mult <=  n; mult++)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
 				prod = num * mult;
+
 				if (prod <= 99)
 					_putchar(' ');
-						if (prod <= 9)
-							_putchar(' ');
-if (prod >= 100)
-	_putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0');
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			if (prod <= 99 && prod >= 10)
-			{
-				_putchar((prod / 10) + '0');
-			}
-			_putchar((prod % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
-		}
+	}
 }
